226969ef43ae41a8f77eb6a814cebd4d8121d2a3
username goes here <pavel.nitu@student.kdg.be>
2020-11-24 14:32:23 +0100

    perfectNumber

--- src/main/java/com/exodia0/htfjavamaze/challengeSolvers/ChallengeSolver.java
+++ src/main/java/com/exodia0/htfjavamaze/challengeSolvers/ChallengeSolver.java
@@ -1,10 +1,9 @@
 package com.exodia0.htfjavamaze.challengeSolvers;
 
-import org.springframework.stereotype.Component;
 
 
 public interface ChallengeSolver {
 
-    String solve(String parameter);
+    Object solve(String parameter);
     String getChallengeString();
 }
