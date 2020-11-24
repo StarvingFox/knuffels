06979307d7fcf2364320d209d1938a5c0b5eef4d
MiikkaVG <miikka.vangoethem@student.kdg.be>
2020-11-24 14:51:18 +0100

    added code to start adding answers

--- src/main/java/com/exodia0/htfjavamaze/services/SolverService.java
+++ src/main/java/com/exodia0/htfjavamaze/services/SolverService.java
@@ -1,7 +1,9 @@
 package com.exodia0.htfjavamaze.services;
 
 import com.exodia0.htfjavamaze.domain.Cell;
+import com.exodia0.htfjavamaze.domain.CellAnswer;
 import com.exodia0.htfjavamaze.domain.Maze;
+import com.exodia0.htfjavamaze.domain.MazeAnswer;
 import com.exodia0.htfjavamaze.webclients.MazeHttpClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -25,15 +27,20 @@ public class SolverService {
     public void solve(){
         while(true) {
             Maze maze = httpClient.getMaze();
-            //TODO: solve challenges for best path
             List<Cell> path = pathFinder.findPath(maze);
-
+            MazeAnswer answer = new MazeAnswer();
             if (path.isEmpty()) {
                 log.info("No path");
             }
             for (Cell cell : path) {
-                log.info(String.format("[ CELL ] x: %d, y: %d", cell.getX(), cell.getY()));
+                if(cell.isDecisionPoint()){
+
+                }else{
+                    answer.getCells().add(new CellAnswer(null, null, cell.getX(), cell.getY()));
+                }
             }
+
+            httpClient.postAnswer(maze.getMazeId(), answer);
         }
     }
 }
