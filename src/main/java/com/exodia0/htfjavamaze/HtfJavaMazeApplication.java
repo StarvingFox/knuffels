6cf4729cbe9a72cb30d783b6d8d6a0c04ad1cd1c
MiikkaVG <miikka.vangoethem@student.kdg.be>
2020-11-24 09:47:21 +0100

    initial commit :seedling:

---
+++ src/main/java/com/exodia0/htfjavamaze/HtfJavaMazeApplication.java
@@ -0,0 +1,13 @@
+package com.exodia0.htfjavamaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtfJavaMazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtfJavaMazeApplication.class, args);
+	}
+
+}
