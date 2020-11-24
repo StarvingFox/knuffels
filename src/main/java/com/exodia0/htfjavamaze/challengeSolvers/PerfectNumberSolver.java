a7a33ed7e1d2b54bf935fb0b57cb73483510bc3e
username goes here <pavel.nitu@student.kdg.be>
2020-11-24 14:32:23 +0100

    perfectNumber

---
+++ src/main/java/com/exodia0/htfjavamaze/challengeSolvers/PerfectNumberSolver.java
@@ -0,0 +1,37 @@
+package com.exodia0.htfjavamaze.challengeSolvers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class PerfectNumberSolver implements ChallengeSolver {
+    @Override
+    public Boolean solve(String parameter) {
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(parameter);
+        int numebr = 0;
+        while (m.find()){
+            numebr =  Integer.parseInt(m.group());
+            return perfectNumber(numebr);
+        }
+        return null;
+    }
+
+    @Override
+    public String getChallengeString() {
+        return "What is the Perfect Number in the nth position (1-based)?";
+    }
+
+    public boolean perfectNumber(double number){
+        int sum = 0;
+        for(int i = 1; i<= number/2; i++){
+            if(number%(double)i == 0){
+                sum+=i;
+            }
+        }
+        return (double) sum == number;
+    }
+}
--- src/test/java/com/exodia0/htfjavamaze/TestChallanges.java
+++ src/test/java/com/exodia0/htfjavamaze/TestChallanges.java
@@ -1,6 +1,7 @@
 package com.exodia0.htfjavamaze;
 
 import com.exodia0.htfjavamaze.challengeSolvers.DecodeStringSolver;
+import com.exodia0.htfjavamaze.challengeSolvers.PerfectNumberSolver;
 import com.exodia0.htfjavamaze.challengeSolvers.ReverseStringSolver;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,10 @@ public class TestChallanges {
     @Autowired
     private DecodeStringSolver decodeStringSolver;
 
+    @Autowired
+    private PerfectNumberSolver perfectNumberSolver;
+
+
     @Test
     void testReverseString(){
         assertEquals("akkiim", reverseStringSolver.solve("miikka"));
@@ -25,6 +30,11 @@ public class TestChallanges {
     @Test
     void testDecodeString(){
         assertEquals("AUzfU2IfQdRSrLsh21kyOz84APvrcCP", decodeStringSolver.solve("QVV6ZlUySWZRZFJTckxzaDIxa3lPejg0QVB2cmNDUA=="));
+    }
+
+    @Test
+    void testPerfectNumber(){
+        assertEquals(true, perfectNumberSolver.solve("{\"nth element\":\"28\"}"));
     }
 
 
